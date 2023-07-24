d116b702db4aa8e187981a490988d584052e3357
PTrinh202 <118448037+PTrinh202@users.noreply.github.com>
2023-07-24 23:56:54 +0700

    Add files via upload

---
+++ Buổi 2/Buoi2_Cau4.cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	int so, tong = 0;
+	cout<<"Nhap so: ";
+	cin>>n;
+	so = n;
+	while(so != 0){
+		tong += so % 10;
+		so /= 10;
+		}
+	cout<<"Tong cua "<<n<<" la "<<tong<<endl;
+	return 0;
+}
